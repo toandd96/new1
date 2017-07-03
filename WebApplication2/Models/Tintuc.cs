e60194cd5122abf3635ea262df1d3b829989138e
daodu <toandd@hiworld.com.vn>
2017-07-03 11:32:34 +0700

    toandd

--- WebApplication2/Models/Tintuc.cs
+++ WebApplication2/Models/Tintuc.cs
@@ -7,23 +7,29 @@ namespace WebApplication2.Models
     public partial class Tintuc
     {
         public int Matintuc { get; set; }
+
         [Required,Display(Name ="Tiêu đề")]
         public string Tieude { get; set; }
+
         [Required, Display(Name = "Tiêu đề con")]
         public string Tieudecon { get; set; }
+
         [Required, Display(Name = "Nội dung")]
         public string Noidung { get; set; }
-        [Required(ErrorMessage = "Chưa có ảnh đâu"), Display(Name = "Ảnh")]
+
+         [Required(ErrorMessage = "Chưa có ảnh đâu"), Display(Name = "Ảnh")]
         public string Anh { get; set; }
+
         [Display(Name ="Ngày đăng")]
         public DateTime? Ngaydang { get; set; }
+
         [Required, Display(Name = "Tác giả")]
         public string Tacgia { get; set; }
+
         [Required, Display(Name = "Mã chuyên mục")]
         public int? Machuyenmuc { get; set; }
 
         [Display(Name ="Mã chuyên mục")]
-        
         public virtual Chuyenmuc MachuyenmucNavigation { get; set; }
     }
 }
